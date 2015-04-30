7ecbc68a3959b5bfaf8d3022b5f56bd7060825fb
Mauricio Melo <mauricio@mauriciomelo.com.br>
2015-04-30 15:51:59 -0300

    Adicionando link para embed

--- lib/google_drive.rb
+++ lib/google_drive.rb
@@ -39,12 +39,23 @@ module Service
         :body_object => new_permission,
         :parameters => { 'fileId' => result.data.id }
       )
-      {link:result.data.webContentLink, thumbnail: get_thumbnail(file_name), title: result.data.title, basename: remove_extension(result.data.title)}
+
+      {
+        link:result.data.webContentLink,
+        thumbnail: get_thumbnail(file_name),
+        title: result.data.title,
+        basename: remove_extension(result.data.title),
+        embed: embed(result.data.webContentLink)
+      }
     end
 
     private
     def remove_extension file_name
       File.basename(file_name, File.extname(file_name))
+    end
+
+    def embed file_name
+      file_name.gsub('&export=download', '')
     end
 
     def get_thumbnail file_name
